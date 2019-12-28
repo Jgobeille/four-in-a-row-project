688e03d3cc3b682b2b03a915e042f5f660240c0a
Jamie Gobeille <djent34@gmail.com>
2019-12-28 16:29:52 -0500

    added createSpaces method to board class

--- four-in-a-row-project/js/Board.js
+++ four-in-a-row-project/js/Board.js
@@ -2,6 +2,32 @@ class Board {
   constructor() {
     this.rows = 6;
     this.columns = 7;
-    this.spaces = [];
+    this.spaces = this.createSpaces();
+  }
+
+  /**
+   * Generates 2D array of spaces.
+   * @return  {Array}     An array of space objects
+   */
+
+  /*
+   Info on how to make nested array:
+   https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
+
+   */
+  createSpaces() {
+    const spaces = [];
+
+    for (var x = 0; x < this.columns; x++) {
+      const column = [];
+
+      for (var y = 0; y < this.rows; y++) {
+        const space = new Space(x, y);
+        column.push(space);
+      }
+      spaces.push(column);
+    }
+    console.log(spaces);
+    return spaces;
   }
 }
